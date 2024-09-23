25bd636b4eb95cd0da4c0c8f5c81354412de3478
JuanEndl <juanendl-_@hotmail.com>
2024-09-23 20:12:16 -0300

    first commint

---
+++ clase 1/print.py
@@ -0,0 +1,15 @@
+#1) la linea de codigo de comenta con
+
+# lajslkdjalsdkjsaldjlasdslajkdad
+
+#2) para un texto grande se comenta con triple # '''
+
+'''
+lakjshdlaskjdaslkjd
+kljashdaslkjdaslkjd
+kljasdlaskjdalsjkd
+lñkajdlañskjdalskñjd
+ñlkasdlñaskjdlñaskjd
+'''
+
+print("Hola mundo")
---
+++ clase 1/sintaxis_basica.py
@@ -0,0 +1,13 @@
+# se declara una variable con nombre (edad)
+# asigmar una variable 
+edad = 45
+
+print("hola tengo" ,edad)
+
+# La coma separa valores ","
+
+#################################################################
+
+edadDeHoy = 54
+
+print("esta es tu edad a en 10 años", edadDeHoy + 10 )
