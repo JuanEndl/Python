811e9067d0072440c21b5c182696dc8830a72bea
JuanEndl <juanendl-_@hotmail.com>
2024-10-29 21:34:25 -0300

    cambios para entrega 1

--- clase 8/entrega1.py
+++ clase 8/entrega1.py
@@ -2,7 +2,7 @@
 
 '''
     producuto
-    [nombre(str),precio(float),cantidad(int)]
+    [nombre(str),cantidad(int),precio(float)]
 '''
 
 productos = []
@@ -22,38 +22,71 @@ while True:
     # Solicitar al usuario que seleccione una opción
     #**************************************************************************
 
-    opcion = int(input("Por favor, selecciona una opción (1-0): "))
-
+    # valida si el usuario coloca un numero o una letra
+    while True:
+        try:
+            opcion = int(input("Por favor, selecciona una opción (0-4): "))
+            break
+        except ValueError:
+            print("Por favor, ingresa un número válido.")
+            continue
+    
+    # cierra el programa
     if opcion == 0:
         print("Nos vemos !!")
         break
+
+    # comienza a usar el programa
     elif opcion == 1:
         print("Ingresar Producto: ")
         nombre = input("Ingresar nombre: ")
-        cantidad = int(input("Ingresar cantidad: "))
-        precio = float(input("Ingresar precio: "))
 
+        # Validad cantidad
+        while True:
+            try:
+                cantidad = int(input("Ingresar cantidad: "))
+                break
+            except ValueError:
+                print("Por Favor, Ingresar un numero correcto")
+            
 
-            # se arma el producto
+        # Validad precio
+        while True:
+            try:
+                precio = float(input("Ingresar precio: "))
+                break
+            except ValueError:
+                print("Por Favor, ingresar un valir correcto")
+            
+                
 
+        # crear un producto y añadirlo a la Lista
         producto = [nombre,cantidad,precio]
         productos.append(producto)
 
+        print(f"Producto {producto[0]} agredado correctamente.\n")
+
+    # todavia nada
     elif opcion == 2:
         print()
+    # todavia nada
     elif opcion == 3:
         print()
+
+    # muestra el listado y valida si hay algo o no
     elif opcion == 4:
         print("Listado")
         if len(productos) == 0:
             print("no hay nada")
+
+        # si encuentra algo trae el producto y ademas valida si tiene el stock bajo o no
         else:
-            print("nombre".ljust(30) + "precio".ljust(15) + "cantidad".ljust(10) + "comentario".ljust(10)) # -> acomoda los titulos 
+            print("nombre".ljust(30) + "cantidad".ljust(15) + "precio".ljust(10) + "comentario".just(20)) # -> acomoda los titulos
             for elemento in productos:
                 comentario = ""
-                if producto[2] < 5: #-> revisa si tiene bajo el stock
-                    comentario = "Bajo" 
-                print(f"{elemento[0].ljust(30)}{str(elemento[1].ljust(15))}{elemento[2].ljust(10)}{comentario.ljust(10)}")
+                if elemento[1] < 5: #-> revisa si tiene bajo el stock
+                    comentario = "Stock Bajo" 
+                print(f"{elemento[0].ljust(30)}{str(elemento[1]).ljust(15)}{str(elemento[2]).ljust(10)}{comentario.rjust(20)}")
     else:
     # Mostramos el nro de la opción seleccionada
         print(f"Has seleccionado: , {opcion}")
